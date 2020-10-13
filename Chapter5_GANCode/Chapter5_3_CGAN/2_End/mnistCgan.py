e6d18420257c1f79e1c22f963133811779b1db56
Jan Schaffranek <jan.schaffranek@rub.de>
2020-10-13 17:10:41 +0200

    Up

--- Chapter5_GANCode/Chapter5_3_CGAN/2_End/mnistCgan.py
+++ Chapter5_GANCode/Chapter5_3_CGAN/2_End/mnistCgan.py
@@ -5,76 +5,69 @@ import numpy as np
 from tensorflow.keras.layers import Input
 from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import to_categorical
 
-from cifarData import CIFAR10
-from cifarDcganDiscriminator import build_discriminator
-from cifarDcganGenerator import build_generator
+from mnistData import MNIST
+from mnistCganDiscriminator import build_discriminator
+from mnistCganGenerator import build_generator
 
 
 PATH = os.path.abspath("C:/Users/Jan/Dropbox/_Programmieren/UdemyGAN")
-IMAGES_PATH = os.path.join(PATH, "Chapter5_GANCode/Chapter5_2_2_DCGAN_CIFAR/images")
+IMAGES_PATH = os.path.join(PATH, "Chapter5_GANCode/Chapter5_3_CGAN/images")
 
 
-class DCGAN:
+class CGAN:
     def __init__(self):
         # Model parameters
-        self.img_rows = 32
-        self.img_cols = 32
-        self.img_depth = 3
+        self.img_rows = 28
+        self.img_cols = 28
+        self.img_depth = 1
         self.img_shape = (
             self.img_rows,
             self.img_cols,
             self.img_depth
         )
         self.z_dimension = 100
-        optimizer_discriminator = Adam(
-            learning_rate=0.0003
-        )
-        optimizer_generator = Adam(
-            learning_rate=0.0008
+        self.num_classes = 10
+        optimizer = Adam(
+            learning_rate=0.0002,
+            beta_1=0.5
         )
         # Build Discriminator
         self.discriminator = build_discriminator(
-            img_shape=self.img_shape
+            img_shape=self.img_shape,
+            num_classes=self.num_classes
         )
         self.discriminator.compile(
             loss="binary_crossentropy",
-            optimizer=optimizer_discriminator,
+            optimizer=optimizer,
             metrics=["accuracy"]
         )
         # Build Generator
         self.generator = build_generator(
             z_dimension=self.z_dimension,
-            img_shape=self.img_shape
+            img_shape=self.img_shape,
+            num_classes=self.num_classes
         )
-        z = Input(shape=(self.z_dimension,)) # Input for Generator
-        img = self.generator(z) # Generator generates an image
+        noise = Input(shape=(self.z_dimension,)) # Input for Generator
+        label = Input(shape=(self.num_classes,))
+        img = self.generator([noise, label]) # Generator generates an image
         self.discriminator.trainable = False # Set the discriminator in non-trainable mode
-        d_pred = self.discriminator(img) # Generator image as input for the discriminator
+        d_pred = self.discriminator([img, label]) # Generator image as input for the discriminator
         self.combined = Model(
-            inputs=z,
+            inputs=[noise, label],
             outputs=d_pred
         )
         self.combined.compile(
             loss="binary_crossentropy",
-            optimizer=optimizer_generator,
+            optimizer=optimizer,
             metrics=[]
         )
 
-    def train_generator(self, noise, y_real):
-        g_loss = self.combined.train_on_batch(x=noise, y=y_real)
-        return g_loss
-
-    def train_discriminator(self, train_imgs, generated_imgs, y_real, y_fake, smooth=0.02):
-        d_loss_real = self.discriminator.train_on_batch(x=train_imgs, y=y_real * (1.0 - smooth))
-        d_loss_fake = self.discriminator.train_on_batch(x=generated_imgs, y=y_fake)
-        d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-        return d_loss
-
     def train(self, epochs, batch_size, sample_interval):
-        # Load and rescale dataset
-        cifar_data = CIFAR10()
-        x_train, _ = cifar_data.get_train_set()
+        # Load and resacle data
+        mnist_data = MNIST()
+        x_train, y_train = mnist_data.get_train_set()
         x_train = (x_train / 127.5) - 1.0
         # Adverserial ground truths
         y_real = np.ones(shape=(batch_size, 1))
@@ -85,20 +78,24 @@ class DCGAN:
             # Trainset images
             rand_idxs = np.random.randint(0, x_train.shape[0], batch_size)
             train_imgs = x_train[rand_idxs]
+            train_labels = y_train[rand_idxs]
             # Generated images
             noise = np.random.normal(loc=0.0, scale=1.0, size=(batch_size, self.z_dimension))
-            generated_imgs = self.generator(noise, training=False)
-            # Training
-            d_loss = self.train_discriminator(train_imgs, generated_imgs, y_real, y_fake)
-            g_loss = self.train_generator(noise, y_real)
+            generated_imgs = self.generator([noise, train_labels], training=False)
+            # Train the discriminator
+            d_loss_real = self.discriminator.train_on_batch([train_imgs, train_labels], y_real)
+            d_loss_fake = self.discriminator.train_on_batch([generated_imgs, train_labels], y_fake)
+            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            # Train the generator
+            noise = np.random.normal(loc=0.0, scale=1.0, size=(batch_size, self.z_dimension))
+            g_loss = self.combined.train_on_batch([noise, train_labels], y_real)
+            # Save the progress
             if (epoch % sample_interval) == 0:
                 print(
                     f"{epoch} - D_loss: {round(d_loss[0], 4)}"
                     f" D_acc: {round(d_loss[1], 4)}"
                     f" G_loss: {round(g_loss, 4)}"
                 )
-            # Save the progress
-            if (epoch % sample_interval) == 0:
                 self.sample_images(epoch)
         self.sample_images("final")
 
@@ -110,15 +107,18 @@ class DCGAN:
         epoch : int
             Number of the current epoch
         """
-        r, c = 5, 5
+        r, c = 2, 5
         noise = np.random.normal(loc=0.0, scale=1.0, size=(r * c, self.z_dimension))
-        gen_imgs = self.generator.predict(noise)
+        labels = np.random.randint(0, self.num_classes, 10)
+        labels_categorical = to_categorical(labels, num_classes=self.num_classes)
+        gen_imgs = self.generator.predict([noise, labels_categorical])
         gen_imgs = 0.5 * gen_imgs + 0.5
         fig, axs = plt.subplots(r, c)
         cnt = 0
         for i in range(r):
             for j in range(c):
-                axs[i, j].imshow(gen_imgs[cnt, :, :])
+                axs[i, j].imshow(gen_imgs[cnt, :, :, 0], cmap="gray")
+                axs[i, j].set_title(f"Digit: {labels[cnt]}")
                 axs[i, j].axis("off")
                 cnt += 1
         img_name = f"{epoch}.png"
@@ -127,9 +127,9 @@ class DCGAN:
 
 
 if __name__ == "__main__":
-    dcgan = DCGAN()
-    dcgan.train(
-        epochs=50_000,
-        batch_size=32,
+    cgan = CGAN()
+    cgan.train(
+        epochs=20_000,
+        batch_size=128,
         sample_interval=1_000
     )
---
+++ Chapter5_GANCode/Chapter5_3_CGAN/2_End/mnistCganGenerator.py
@@ -0,0 +1,41 @@
+import numpy as np
+from tensorflow.keras.layers import Activation
+from tensorflow.keras.layers import BatchNormalization
+from tensorflow.keras.layers import Concatenate
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Input
+from tensorflow.keras.layers import LeakyReLU
+from tensorflow.keras.layers import Reshape
+from tensorflow.keras.models import Model
+
+
+def build_generator(z_dimension, img_shape, num_classes):
+    noise = Input(shape=(z_dimension,))
+    label = Input(shape=(num_classes,))
+    x = Concatenate()([noise, label])
+    x = Dense(units=512)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = BatchNormalization()(x)
+    x = Dense(units=1024)(x)
+    x = LeakyReLU(alpha=0.2)(x)
+    x = BatchNormalization()(x)
+    x = Dense(np.prod(img_shape))(x)
+    x = Activation("tanh")(x)
+    img = Reshape(img_shape)(x)
+    model = Model(
+        inputs=[noise, label],
+        outputs=img
+    )
+    model.summary()
+    return model
+
+
+if __name__ == "__main__":
+    z_dimension = 100
+    img_shape = (28, 28, 1)
+    num_classes = 10
+    model = build_generator(
+        z_dimension=z_dimension,
+        img_shape=img_shape,
+        num_classes=num_classes
+    )
